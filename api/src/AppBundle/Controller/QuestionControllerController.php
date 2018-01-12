a6dd9462006b646538466f961ea5b44f07d64721
Corey Kress <corey.kress@coachmeplus.com>
2018-01-12 09:31:54 -0500

    added react app and question controller

---
+++ api/src/AppBundle/Controller/QuestionControllerController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class QuestionControllerController extends Controller
+{
+    function indexAction(Request $request)
+    {
+        return new Response('test');
+//        // replace this example code with whatever you need
+//        return $this->render('default/index.html.twig', [
+//            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+//        ]);
+    }
+}
